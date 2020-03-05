238e088d779b28e51fb9a01c683db8b76d3ce343
Shahadan Bin Saad <shahadansaad@gmail.com>
2020-03-05 11:49:49 +0800

    Changes to be committed: 	modified:   lib/connect.php 	modified:   lima.php

--- lib/connect.php
+++ lib/connect.php
@@ -2,14 +2,14 @@
 
 require_once 'config.php';
 
-$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+$conn = new mysqli( DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME );
+if ( $conn->connect_error ) {
+    die( 'Connection failed: ' . $conn->connect_error );
+}
 
-function close_connection(){
-	global $conn;
-	mysqli_close($conn);
+function close_connection() {
+    global $conn;
+    mysqli_close( $conn );
 }
 
 ?>
--- lima.php
+++ lima.php
@@ -1,57 +1,89 @@
 <?php
-require('lib/connect.php');
+require( 'lib/connect.php' );
 $userid = 0;
 
+function logsir( $current_log, $current_status ) {
+    global $userid;
+    global $conn;
 
-$sir = 1;
-// A list of permitted file extensions
-$allowed = array('png', 'jpg', 'gif' , 'zip', 'mp4', 'srt', 'pptx', 'txt');
+    $sql0 = "INSERT INTO logs (all_user_id, all_logs, all_status) VALUES ($userid, '$current_log', '$current_status')";
 
-if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
+    if ( $conn->query( $sql0 ) === TRUE ) {
+        echo 'New record created successfully';
+    } else {
+        echo 'Error: ' . $sql0 . '<br>' . $conn->error;
+    }
 
-	if($sir){
-		
-		$newname = md5_file($_FILES['upl']['tmp_name']);
-
-		$sql = "SELECT COUNT(*) AS total_count FROM files WHERE md5_checksum = ".$newname;
-		$result=mysqli_query($con,$sql);
-		$data=mysqli_fetch_assoc($result);
-		//echo $data['total']; 
-		if($data['total']){
-
-		$directoryName = 'F:\sharedata/alldata/'.substr($newname, 0, 1).'/'.substr($newname, 0, 2).'/'.substr($newname, 0, 3);
- 
-		//Check if the directory already exists.
-		if(!is_dir($directoryName)){
-    		//Directory does not exist, so lets create it.
-    		mkdir($directoryName, 0755, true);
-		}
-
-		if(move_uploaded_file($_FILES['upl']['tmp_name'], $directoryName.'/'.$newname)){
-			
-			echo '{"status":"success"}';
-			exit;
-		}
-		}
-		else{
-			echo $data['total'];
-		}	
-
-	}
-	else{
-	$extension = pathinfo($_FILES['upl']['name'], PATHINFO_EXTENSION);
-
-	if(!in_array(strtolower($extension), $allowed)){
-		echo '{"status":"error"}';
-		exit;
-	}
-
-	if(move_uploaded_file($_FILES['upl']['tmp_name'], 'uploads/'.$_FILES['upl']['name'])){
-		echo '{"status":"success"}';
-		exit;
-	}
-	}
 }
 
+$sir = 1;
+logsir( 'baca sir = 1', 'starting' );
+// A list of permitted file extensions
+$allowed = array( 'png', 'jpg', 'gif', 'zip', 'mp4', 'srt', 'pptx', 'txt' );
+
+if ( isset( $_FILES['upl'] ) && $_FILES['upl']['error'] == 0 ) {
+    logsir( 'baca isset', 'loading' );
+    if ( $sir ) {
+        logsir( 'baca sir == 1', 'loading' );
+
+        $newname = md5_file( $_FILES['upl']['tmp_name'] );
+
+        $sql1 = 'SELECT COUNT(*) AS total_count FROM files WHERE md5_checksum = '.$newname;
+        $result = mysqli_query( $conn, $sql1 );
+        $data = mysqli_fetch_assoc( $result );
+        //echo $data['total_count'];
+
+        logsir( 'md5 = '.$newname, 'success' );
+        logsir( 'count existing md5 = '.$data['total_count'], 'success' );
+
+        if ( true ) {
+            //if ( $data['total_count'] == 0 ) {
+            //    echo $data['total_count'];
+            //} else if ( $data['total_count'] == 0 ) {
+            $directoryName = 'H:\sharedata/alldata/'.substr( $newname, 0, 1 ).'/'.substr( $newname, 0, 2 ).'/'.substr( $newname, 0, 3 );
+
+            //Check if the directory already exists.
+            if ( !is_dir( $directoryName ) ) {
+                //Directory does not exist, so lets create it.
+                mkdir( $directoryName, 0755, true );
+            }
+
+            $sql2 = "INSERT INTO testingg (apa_apa_jer) VALUES ('Malu Apa Bossku')";
+
+            if ( $conn->query( $sql2 ) === TRUE ) {
+                echo 'New record created successfully';
+            } else {
+                echo 'Error: ' . $sql . '<br>' . $conn->error;
+            }
+
+            if ( move_uploaded_file( $_FILES['upl']['tmp_name'], $directoryName.'/'.$newname ) ) {
+
+                echo '{"status":"success"}';
+                exit;
+            }
+
+        } else {
+            echo 'error bossku';
+            exit;
+        }
+
+    } else {
+        logsir( 'baca sir != 1', 'loading' );
+        $extension = pathinfo( $_FILES['upl']['name'], PATHINFO_EXTENSION );
+
+        if ( !in_array( strtolower( $extension ), $allowed ) ) {
+            echo '{"status":"error"}';
+            exit;
+        }
+
+        if ( move_uploaded_file( $_FILES['upl']['tmp_name'], 'uploads/'.$_FILES['upl']['name'] ) ) {
+            echo '{"status":"success"}';
+            exit;
+        }
+        $conn->close();
+    }
+}
+
+logsir( 'no loading', 'error' );
 echo '{"status":"error"}';
 exit;
